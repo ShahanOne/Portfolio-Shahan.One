e4a4d7c389bd990cacc6902371d9ccd0ed1f3b00
Shahan.One <shahanhussain9105@gmail.com>
2023-06-20 01:19:41 +0530

    rem api

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,17 @@
 import '../styles/globals.css';
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
-import Head from 'next/head';
 export const metadata = {
   title: 'Shahan.One Portfolio',
   description: 'Portfolio for Syed Shahan Hussain',
+  icons: {
+    icon: '/dev.ico',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="dev.ico" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
